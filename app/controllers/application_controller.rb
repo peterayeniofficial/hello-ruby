452c3433b48d4190fa6dd63fa11fceb6ad493e70
Peter Ayeni <ayenicology@gmail.com>
2019-07-28 13:04:38 +0100

    add a route

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def hello
+    render html: "Hello, World" 
+  end
+
+  def goodbye
+    render html: "¡Good Bye, World!"
+  end
 end
